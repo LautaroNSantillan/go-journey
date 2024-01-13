91c8ad8b1a168219351528bd6ce5f2fed453bc74
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-01-12 22:52:19 -0300

    arrays&slices

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import "fmt"
 func main() {
 
 	//var & consts
-	bookingAppName := "The greatest Go booking app ever"
+	bookingAppName := "The greatest Go booking app ever" //syntactic sugar
 	const totalTickets = 50
 	var remainingTickets uint = 50
+
+	//array & slices
+	bookings := []string{} // no size = slice
 
 	fmt.Printf("bookingAppName is %T, totalTickets is %T, remainingTickets is %T\n", bookingAppName, totalTickets, remainingTickets)
 
@@ -17,8 +20,29 @@ func main() {
 	fmt.Printf("Total tickets: %v, Available tickets: %v\n", remainingTickets, totalTickets)
 
 	//data types
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	fmt.Printf("User %v has booked %v tickets", userName, userTickets)
+	//user input
+	fmt.Println("Please enter your first name")
+	fmt.Scan(&firstName) //& pointer to where the var is stored in memory
+
+	fmt.Println("Please enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email")
+	fmt.Scan(&email)
+
+	fmt.Println("Please enter your amount of desired tickets")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive your order details at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remainig for %v.\n", remainingTickets, bookingAppName)
+
+	fmt.Printf("All bookings: %v\n", bookings)
 }
