40fedd4a1b0e739248dfa08254572a568c984383
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-01-15 21:25:58 -0300

    CRUD done

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -36,10 +38,10 @@ func main() {
 	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server on port 8080\n")
-	log.Fatal(http.ListenAndServe("8080", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func gerMovies(res http.ResponseWriter, req *http.Request) {
+func getMovies(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(res).Encode(movies)
 }
@@ -53,6 +55,54 @@ func deleteMovie(res http.ResponseWriter, req *http.Request) {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
 			break
+		}
+	}
+	json.NewEncoder(res).Encode(movies)
+}
+
+func getMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(req)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(res).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	var movie Movie
+
+	_ = json.NewDecoder(req.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000))
+
+	movies = append(movies, movie)
+
+	json.NewEncoder(res).Encode(movie)
+}
+
+func updateMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(req)
+
+	for index, movie := range movies {
+		if movie.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+
+			var newMovie Movie
+		_:
+			json.NewDecoder(req.Body).Decode(&newMovie)
+			newMovie.ID = params["id"]
+
+			movies = append(movies, newMovie)
+
+			json.NewEncoder(res).Encode(newMovie)
 		}
 	}
 }
