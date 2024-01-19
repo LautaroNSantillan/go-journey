0331eeb34ef7fa19506a123b177669b7e373549e
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-01-18 22:10:45 -0300

    initDB + .env

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,16 +5,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/LautaroNSantillan/my-go-journey/tree/mysql-books/pkg/models"
 	"github.com/LautaroNSantillan/my-go-journey/tree/mysql-books/pkg/routes"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+	models.InitDB()
 	router := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(router)
 
 	http.Handle("/", router)
-	fmt.Printf("Starting server on port 8080\n")
-	log.Fatal(http.ListenAndServe("localhost:8080", router))
+	fmt.Printf("Starting server on port 8081\n")
+	log.Fatal(http.ListenAndServe("localhost:8081", router))
 }
