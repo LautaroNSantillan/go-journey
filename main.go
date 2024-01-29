d59c68bdca555bdc4818d07c43cd8de022efd2b4
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-01-29 11:46:15 -0300

    htmx

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"text/template"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -10,10 +11,12 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	r.Get("/", welcome)
 	http.ListenAndServe(":3000", r)
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome"))
+	tmpl, _ := template.New("").ParseFiles("templates/index.html")
+	tmpl.ExecuteTemplate(w, "Base", nil)
 }
